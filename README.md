8c979fa2b305d7465a9d2f3210145cb5d097911d
西村　健亮 <knkn.kenken@gmail.com>
2020-09-07 00:15:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+参照：大幅にリニューアルされた Next.js のチュートリアルをどこよりも早く全編和訳しました
+
+https://qiita.com/thesugar/items/01896c1faa8241e6b1bc
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <b>Kensuke</b>. I'm developer, designer in DAICHI GROUP. You can contact me on <a href="https://twitter.com/knk_n/">Twitter</a>.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
